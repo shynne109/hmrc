8d7c9b59af890f77f54852f9a74deb59dd488d35
Adedoyin Shina <shynne109@gmail.com>
2025-11-15 10:27:15 +0100

    refactor: Improve Composer autoloader path resolution in bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,31 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-// composer autoloader
-$autoloader = require __DIR__.'/../vendor/autoload.php';
+// composer autoloader - find the correct path depending on installation context
+$autoloaderPath = null;
+
+// First check if this is a standalone installation (normal case)
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    $autoloaderPath = __DIR__ . '/../vendor/autoload.php';
+}
+// Otherwise check if this is a composer-managed package (vendor/shynne109/hmrc case)
+// Look for autoloader in ../../.. (vendor/vendor/autoload.php doesn't exist, so we go up to root project)
+elseif (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    $autoloaderPath = __DIR__ . '/../../../../vendor/autoload.php';
+}
+// Fallback for other structures
+elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    $autoloaderPath = __DIR__ . '/../../../autoload.php';
+}
+
+if (!$autoloaderPath) {
+    die('Could not find Composer autoloader. Expected it at one of: ' . "\n" .
+        __DIR__ . '/../vendor/autoload.php' . "\n" .
+        __DIR__ . '/../../../../vendor/autoload.php' . "\n" .
+        __DIR__ . '/../../../autoload.php' . "\n");
+}
+
+$autoloader = require $autoloaderPath;
 $autoloader->add('GovTalk\\GiftAid\\',__DIR__);
 
 // Explicitly require PAYE TestCase so names can resolve
